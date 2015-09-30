3e505f47c9dd841893f93731eee0089ead6c6eb4
Corey Burmeister <burmeister.corey@gmail.com>
2015-09-29 20:01:28 -0700

    wip

--- regina.py
+++ regina.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python
 
-import os
 import re
-import time
+import csv
 import click
+import shutil
 import requests
 from bs4 import BeautifulSoup
+from itertools import izip_longest
 
-genres = [
-    'house',
-    'deep-house',
-    'techno',
-    'minimal-tech-house',
-]
 
-windows = [
-    'this-week',
-    'today',
-    'eight-weeks',
-]
-
+genres = ['house', 'deep-house', 'techno', 'minimal-tech-house']
+windows = ['this-week', 'today', 'eight-weeks']
 limits = ['10', '20', '50', '100', '500']
 
 
@@ -28,37 +19,82 @@ limits = ['10', '20', '50', '100', '500']
 @click.option('--window', type=click.Choice(windows), help='How far back you feel like lookin?')
 @click.option('--limit', type=click.Choice(limits), help='How many releases you finna download?')
 @click.option('--sleep', default=3, help='Be Nice :D')
-@click.option('--directory', default='/tmp', help='Where do you wanna put these files?')
-def main(genre, window, limit, sleep, directory):
+@click.option('--filename', help='Where do you wanna put this file?')
+def main(genre, window, limit, sleep, filename):
     """
     Browse new releases from http://www.juno.co.uk/ without leaving the prompt.
     """
-    r = requests.get('http://www.juno.co.uk/%s/%s/?items_per_page=%s' % (genre, window, limit))
-    soup = BeautifulSoup(r.content)
+    base_url = 'http://www.juno.co.uk/'
+    url = '{}/{}/{}/?items_per_page={}'.format(base_url, genre, window, limit)
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'lxml')
+    table = soup.find(class_='product_list')
+    products = table.find_all(attrs={'ua_location': re.compile('product')})
 
-    urls = soup.findAll('a', href=re.compile('http.*\.mp3'))
+    def grouper(n, iterable, fillvalue=None):
+        args = [iter(iterable)] * n
+        return izip_longest(fillvalue=fillvalue, *args)
 
-    dir_path = os.path.join(directory, genre, window)
+    with open(filename, 'wb') as out_file:
+        csv_writer = csv.writer(out_file)
+        csv_writer.writerow([
+            'artist', 'title', 'label', 'catno', 'format', 'genre', 'style',
+            'tracks', 'images',
+        ])
+        for idx, (row1, row2, row3) in enumerate(grouper(3, products)):
+            try:
+                artist = row1\
+                    .find(class_='productartist')\
+                    .find('span')\
+                    .find('a')\
+                    .text
+                title = row1.find(class_='producttitle').find('span').text
+                url = row1.find(class_='producttitle').find('a', class_='jhighlight')['href']
+                label = row1.find(class_='productlabel').find('span').text
+                catno = row2.find('span').text
+                format_ = row1\
+                    .find(class_='producttitle')\
+                    .text.strip('\t\n\r')\
+                    .split('(', 1)[1]\
+                    .split(')')[0]
+                tracks = filter(None, [
+                    x.strip() for x in row3.find('span').text
+                    .encode('utf8')
+                    .replace('\t', '')
+                    .replace('\n', '')
+                    .replace('\xa0', '')
+                    .replace('\xc2', '')
+                    .splitlines()
+                ])
 
-    try:
-        os.makedirs(dir_path)
-    except OSError:
-            pass # already exists
+                r = requests.get(base_url + url)
+                soup2 = BeautifulSoup(r.content, 'lxml')
 
-    for url in [x['href'] for x in urls]:
-        filename = url.rsplit('/', 1)[1]
+                image_keys = []
+                images = soup2.find('div', class_='product-images-large')
+                for image_idx, image in enumerate(images.find_all('img')):
+                    r = requests.get(image['src'], stream=True)
+                    if r.status_code == 200:
+                        key = '{}-{}-{}.jpeg'.format(filename, idx, image_idx)
+                        with open(key, 'wb') as f:
+                            r.raw.decode_content = True
+                            shutil.copyfileobj(r.raw, f)
+                            image_keys.append(key)
 
-        file_path = os.path.join(dir_path, filename)
-        click.echo('Downloading {0} to {1}'.format(url, file_path))
-
-        mp3 = requests.get(url, stream=True)
-
-        with open(file_path, 'wb') as fd:
-            for chunk in mp3.iter_content(1024):
-                fd.write(chunk)
-
-        time.sleep(sleep)
+                csv_writer.writerow([
+                    artist,
+                    title,
+                    label,
+                    catno,
+                    format_,
+                    'Electronic',
+                    genre.replace('-', ' ').title(),
+                    ', '.join(tracks),
+                    ', '.join(image_keys),
+                ])
+            except (UnicodeEncodeError, UnicodeDecodeError):
+                continue
 
 
 if __name__ == "__main__":
-   main()
+    main()
